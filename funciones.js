4a6cc43fbb8e72f2dabe90b83d14c44f1637b31e
Clara Hester <clarahester@Claras-MacBook-Air.local>
2020-11-10 15:32:28 -0600

    11-10-20

---
+++ funciones.js
@@ -0,0 +1,9 @@
+console.log("Primera práctica");
+var variableVar = "Variable con Var";
+let variableLet = "Variable con Let";
+const variableConst = "Variable con Const";
+console.log(variableVar);
+console.log(variableLet);
+console.log(variableConst);
+var variableVar = "Variable con Var";
+variableConst = "Nuevo valor de variable const";
